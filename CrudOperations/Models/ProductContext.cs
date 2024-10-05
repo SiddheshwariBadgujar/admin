3cc8cfb2a13b676606c8c30c7276dca6b17e4218
SiddheshwariBadgujar <113290385+SiddheshwariBadgujar@users.noreply.github.com>
2024-10-05 12:35:07 +0530

    Update ProductContext.cs

--- CrudOperations/Models/ProductContext.cs
+++ CrudOperations/Models/ProductContext.cs
@@ -4,11 +4,11 @@ namespace CrudOperations.Models
 {
     public class ProductContext : DbContext
     {
-        public ProductContext() : base("name=CategoryContext") // Ensure this matches your connection string name
+        public ProductContext() : base("name=CategoryContext") 
         {
         }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; } // Add this line to access categories
+        public DbSet<Category> Categories { get; set; } 
     }
 }
